c0f6021afb2ba51f7158d3e4b71ed00a25532404
andreayag <ayag97@gmail.com>
2021-08-10 00:33:19 -0300

    adding the code for api.py and item.py

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import dataclasses
+import json
 
-from flask import Flask, jsonify
-from flask import request
+from flask import Flask, jsonify, request
 
 from db import DB
 from itemservice import ItemService, ItemNotFoundException
@@ -27,62 +27,57 @@ def status():
 
 @app.route("/items", methods=['POST'])
 def create_item():
-    #############################
-    #### WRITE YOUR CODE HERE ###
-    #############################
+    itemservice = ItemService()
+    description = json.loads(request.data)['description']
+    if not description:
+        return {}, 400
 
-    #############################
-    return jsonify(dataclasses.asdict(item)), status_code
-
+    item = itemservice.create(description)
+    if not item:
+        return {}, 500
+    
+    return jsonify(dataclasses.asdict(item)), 201
+    
 
 @app.route("/items", methods=['GET'])
 def list_items():
-    #############################
-    #### WRITE YOUR CODE HERE ###
-    #############################
+    itemservice = ItemService()
+    amount = request.args['amount']
+    order_by = request.args['order_by']
+    order = request.args['order']
+    if not amount or not order_by or not order:
+        return {}, 400
 
-    #############################
-    return jsonify([dataclasses.asdict(item) for item in items]), status_code
+    items = itemservice.list(amount, order_by, order)
+    if not items:
+        return {}, 500
+
+    return jsonify([dataclasses.asdict(item) for item in items]), 200
 
 
 @app.route("/items/<item_id>", methods=['GET'])
 def get_item(item_id):
+    itemservice = ItemService()
     try:
-        #############################
-        #### WRITE YOUR CODE HERE ###
-        #############################
-
-        #############################
+        item = itemservice.find_by_id(item_id)  
         pass
     except ItemNotFoundException:
-        #############################
-        #### WRITE YOUR CODE HERE ###
-        #############################
+    
+        return jsonify({'error_code': 'ITEM_NOT_FOUND'}), 404
 
-        #############################
-        return jsonify({'error_code': 'ITEM_NOT_FOUND'}), status_code
-
-    #############################
-    #### WRITE YOUR CODE HERE ###
-    #############################
-
-    #############################
-    return jsonify(dataclasses.asdict(item)), status_code
+    return jsonify(dataclasses.asdict(item)), 200
 
 
 @app.route("/items/<item_id>", methods=['PUT'])
 def increment_item(item_id):
-    #############################
-    #### WRITE YOUR CODE HERE ###
-    #############################
-
-    #############################
+    itemservice = ItemService()
+    increment = json.loads(request.data)['increment']
     if increment:
-        #############################
-        #### WRITE YOUR CODE HERE ###
-        #############################
-
-        #############################
-        return jsonify(dataclasses.asdict(item)), status_code
+        try:
+            item = itemservice.increment(item_id)
+        except ItemNotFoundException:
+            
+            return jsonify({'error_code': 'ITEM_NOT_FOUND'}), 404
+        return jsonify(dataclasses.asdict(item)), 200
     else:
         return {}, 200
--- item.py
+++ item.py
@@ -1,11 +1,14 @@
 import random
 from dataclasses import dataclass
 from typing import List, Optional
-
+from multiprocessing import Lock
+import time
 import jsons
-
+import  threading
 from db import DB
-
+lo = Lock() 
+''' I added this global variable since increment test works with multiprocessing and every thread needs to know if the record is locked.
+ There's probably a better solution to this, like modifying the test file and adding the lock there but I didn't want to change that file. '''
 
 @dataclass
 class Item(object):
@@ -19,41 +22,62 @@ class ItemDAO(object):
 
     def __init__(self):
         self.db = DB()
+        
 
     def create(self, description: str) -> Item:
-        #############################
-        #### WRITE YOUR CODE HERE ###
-        #############################
 
-        #############################
+        try:
+            cursor = self.db.get_connection()
+            sql = "INSERT INTO item (description, available_amount) VALUES (%s, %s)"
+            val = (description, 0)
+            cursor.execute(sql, val)
+            item_id = cursor.lastrowid
+        except Exception:
+            print("Failed to create new item")
+       
         return Item(item_id, description, 0)
 
-    def list(self, amount=None, order_by=None, order=None) -> List[Item]:
-        #############################
-        #### WRITE YOUR CODE HERE ###
-        #############################
+    def list(self, amount=10, order_by='description', order='DESC') -> List[Item]:
+        
+        try:
+            cursor = self.db.get_connection()
+            sql = "SELECT * from item ORDER BY " + order_by + " " + order + " LIMIT " + str(amount)
+            cursor.execute(sql)
+            records = cursor.fetchall()
 
-        #############################
+        except Exception:
+            print("Failed to list items")
+       
         items = jsons.load(records, List[Item])
         return items
 
     def find_by_id(self, item_id: int, lock=False) -> Optional[Item]:
-        #############################
-        #### WRITE YOUR CODE HERE ###
-        #############################
+        try:
+            cursor = self.db.get_connection()
+            sql = "SELECT * FROM item WHERE id = " + str(item_id)
+            cursor.execute(sql)
+            records = cursor.fetchone()
+            if not records:
+                return None
+            item = jsons.load(records, Item)
+            return item
 
-        #############################
-
-        if not records:
-            return None
-
-        item = jsons.load(records[0], Item)
-        return item
+        except Exception:
+            print("Failed to find item")
+      
 
     def increment(self, item: Item) -> Item:
-        #############################
-        #### WRITE YOUR CODE HERE ###
-        #############################
+        global lo
+        with lo:
+            try:
+                cursor = self.db.get_connection()
+                item_amount = self.find_by_id(item.id).available_amount
+                amount = item_amount + 1
+                sql = "UPDATE item SET available_amount = " + str(amount) + " WHERE id = " + str(item.id)
+                cursor.execute(sql)
+                updated_item = self.find_by_id(item.id)
+                return updated_item
 
-        #############################
-        return item
+            except Exception:
+                print("Failed to increment available amount of item: "+ str(item.description))
+        
